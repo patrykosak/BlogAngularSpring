57d7ad31975d1605c0cc59f6eb1974963afa49e2
Patryk Osak <pionchaoo@gmail.com>
2022-03-31 22:49:00 +0200

    add post entity

---
+++ backend/src/main/java/com/example/blog/entity/Post.java
@@ -0,0 +1,36 @@
+package com.example.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @Lob
+    @NotEmpty
+    private String content;
+
+    private Instant createdOn;
+
+    private Instant updatedOn;
+    
+    @NotBlank
+    private String username;
+
+}
