93687a489779d1675914ae63ac17f17c8c163658
patrykosak <pionchaoo@gmail.com>
2022-05-01 16:34:16 +0200

    encoding passwords

--- backend/src/main/java/com/example/blog/service/AuthServiceImpl.java
+++ backend/src/main/java/com/example/blog/service/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.blog.dto.RegisterRequest;
 import com.example.blog.entity.AppUser;
 import com.example.blog.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,15 +12,21 @@ public class AuthServiceImpl implements AuthService{
 
     @Autowired
     private AppUserRepository appUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void signup(RegisterRequest registerRequest) {
         AppUser appUser = new AppUser();
         appUser.setUserName(registerRequest.getUsername());
-        appUser.setPassword(registerRequest.getPassword());
+        appUser.setPassword(encodePassword(registerRequest.getPassword()));
         appUser.setEmail(registerRequest.getEmail());
         appUserRepository.save(appUser);
 
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 
 
